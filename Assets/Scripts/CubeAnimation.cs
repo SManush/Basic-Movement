e344159ac6d5fe23a4f7fa8d6635abeeb440361d
SManush <manushsimonyan@gmail.com>
2023-12-21 20:13:20 +0400

    - Չգիտեմ ինչ չենջերա

---
+++ Assets/Scripts/CubeAnimation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private bool isAnimating = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isAnimating)
+        {
+            animator.SetBool("isAnimating", true);
+            isAnimating = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && isAnimating)
+        {
+            animator.SetBool("isAnimating", false);
+            isAnimating = false;
+        }
+    }
+}
